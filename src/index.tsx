f18984dcc47e082aaa8ddcbd8f3dfb4acb3edb9a
Donia Robinson <donia@smallpackages.co>
2023-04-10 10:58:41 -0500

    Add lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import { mergeStyles } from "@fluentui/merge-styles";
+import { mergeStyles } from "@fluentui/merge-styles/lib";
 
 // Inject some global styles
 mergeStyles({
